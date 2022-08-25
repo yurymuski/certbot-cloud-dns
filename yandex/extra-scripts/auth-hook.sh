0dac9194563db4eb40f8b8b22a1e732328d8dbbc
Andrei Pavarnitsyn <andrei.pavarnitsyn@scorum.com>
2022-08-25 18:36:20 +0400

    Fix errors and bugs

--- yandex/extra-scripts/auth-hook.sh
+++ yandex/extra-scripts/auth-hook.sh
@@ -2,7 +2,7 @@
 set -e
 
 # set dns TXT record
-YANDEX_DNSZONE_ID=$(yc dns zone list --folder-id $YANDEX_FOLDER_ID --token $YANDEX_TOKEN --format json | jq ".[] | select(.zone == \"$YANDEX_DNS_ZONE.\") | .id")
+YANDEX_DNSZONE_ID=$(yc dns zone list --folder-id $YANDEX_FOLDER_ID --token $YANDEX_TOKEN --format json | jq ".[] | select(.zone == \"$YANDEX_DNS_ZONE.\") | .id" | tr -d \")
 yc dns zone add-records --record "_acme-challenge TXT $CERTBOT_VALIDATION" --id $YANDEX_DNSZONE_ID --folder-id $YANDEX_FOLDER_ID --token $YANDEX_TOKEN 
 
 # w8
