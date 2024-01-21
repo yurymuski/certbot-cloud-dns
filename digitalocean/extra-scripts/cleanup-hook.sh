9d9abdc82c899a342471fbc014695fb72d509b7b
Anatolii Bohatyrenko <anatolii.bohatyrenko@scorum.com>
2024-01-22 00:40:20 +0300

    for to while

--- digitalocean/extra-scripts/cleanup-hook.sh
+++ digitalocean/extra-scripts/cleanup-hook.sh
@@ -4,10 +4,10 @@ set -e
 # doctl login
 doctl auth init ${CERTBOT_DIGITALOCEAN_TOKEN}
 
-# get domain id's
-DOMAIN_LIST=$(doctl compute domain records list ${CERTBOT_DOMAIN} | grep -E '(^|\s)_acme-challenge($|\s)' | awk 'NR > 1 { print $1 }')
+# get record id's
+RECORD_ID_LIST=$(doctl compute domain records list ${CERTBOT_DOMAIN} | grep -E '(^|\s)_acme-challenge($|\s)' | awk 'NR > 1 { print $1 }')
 
-# force delete txt records
-for domains_to_remove in $DOMAIN_LIST; do
-    doctl compute domain records delete ${CERTBOT_DOMAIN} $DOMAIN_LIST -f
+# remove dns TXT record
+echo $RECORD_ID_LIST |  while read -r RECORD_ID; do
+    doctl compute domain records delete ${CERTBOT_DOMAIN} $RECORD_ID_LIST -f
 done
